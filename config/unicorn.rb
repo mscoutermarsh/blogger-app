9d91e55df1bf4a1dc68670f86195b6f8184286a4
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2014-07-27 16:59:44 -0400

    Use unicorn/PG

---
+++ config/unicorn.rb
@@ -0,0 +1,27 @@
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
+
+# kill long running requests. Default: 15 seconds
+# Heroku's default is 30 seconds.
+timeout Integer(ENV['WEB_TIMEOUT'] || 15)
+preload_app true
+
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
