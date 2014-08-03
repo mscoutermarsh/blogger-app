36f1b912b3e8a095b1910086393bdcb9cea5dde8
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2014-08-03 15:14:26 -0400

    Send posts#index to follower. All reads to category go to follower

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Octopus.using(:read_only) {Post.all}
   end
 
   # GET /posts/1
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
+  replicated_model
   has_and_belongs_to_many :posts
   validates_presence_of :name
 end
